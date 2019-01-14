1237fd061ee87486fc9d4693a380d6a218d573f6
Owais Malik <owaismalik@outlook.com>
2019-01-15 02:12:56 +0530

    rock

--- index.php
+++ index.php
@@ -18,34 +18,35 @@ $string = json_decode($twitter->setGetfield($getfield)
 ->performRequest(),$assoc = TRUE);
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <title>TweetOz</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://bootswatch.com/4/spacelab/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    </head>
+<head>
+    <title>TweetOz</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://bootswatch.com/4/spacelab/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
 
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="https://tweetoz.herokuapp.com/">TweetOz</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="https://tweetoz.herokuapp.com/">TweetOz</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="https://tweetoz.herokuapp.com/">Home
-              <span class="sr-only">(current)</span>
-            </a>
-                    </li>
-                    <!-- <li class="nav-item">
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="https://tweetoz.herokuapp.com/">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <!-- <li class="nav-item">
         <a class="nav-link" href="#">Features</a>
       </li>
       <li class="nav-item">
@@ -54,27 +55,28 @@ if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.
       <li class="nav-item">
         <a class="nav-link" href="#">About</a>
       </li> -->
-                </ul>
-                <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                    <input name="user" class="form-control mr-sm-2" type="text" placeholder="Twitter Handle">
-                    <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-        <div class="jumbotron">
-            <h1 class="display-3">Hello, world!</h1>
-            <p class="lead">Here are the latest 10 tweets from
-                <?php echo $user;?>
-            </p>
-            <hr class="my-4">
+            </ul>
+            <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo htmlspecialchars($_SERVER["
+                PHP_SELF"]);?>">
+                <input name="user" class="form-control mr-sm-2" type="text" placeholder="Twitter Handle">
+                <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-        <div class="container">
-            <div class="well">
-                <div class="row">
-                    <?php
+    </nav>
+    <div class="jumbotron">
+        <h1 class="display-3">Hello, world!</h1>
+        <p class="lead">Here are the latest 10 tweets from
+            <?php echo $user;?>
+        </p>
+        <hr class="my-4">
+    </div>
+    <div class="container">
+        <div class="well">
+            <div class="row">
+                <?php
 foreach($string as $items)
     { ?>
-                        <div class="col-sm-6 col-md-3">
+                <!-- <div class="col-sm-6 col-md-3">
                             <div class="card text-white bg-primary mb-3" style="width: 250px; height: 400px;">
                                 <div class="card-header">Tweet</div>
                                 <div class="card-body">
@@ -90,9 +92,19 @@ foreach($string as $items)
                                     </div>
                                 </div>
                             </div>
+                        </div> -->
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $items['user']['name'];?></h5>
+                            <p class="card-text"> <?php echo $items['text'];?></p>
                         </div>
+                        <div class="card-footer">
+                            <small class="text-muted"><?php echo "Time and Date: ".$items['created_at'];?></small>
+                        </div>
+                    </div>
 
-                        <!-- echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+                    <!-- echo "Time and Date of Tweet: ".$items['created_at']."<br />";
         echo "Tweet: ". $items['text']."<br />";
         echo "Image URL: ". $items['user']['profile_image_url']."<br />";
         echo "Tweeted by: ". $items['user']['name']."<br />";
@@ -100,7 +112,7 @@ foreach($string as $items)
         echo "Followers: ". $items['user']['followers_count']."<br />";
         echo "Friends: ". $items['user']['friends_count']."<br />";
         echo "Listed: ". $items['user']['listed_count']."<br />"; -->
-                        <?php 
+                    <?php 
                        
                       }?>
                 </div>
@@ -109,6 +121,6 @@ foreach($string as $items)
                 </p>
             </div>
         </div>
-    </body>
+</body>
 
-    </html>
+</html>
